0255898f63ea871ab5972722125e289ea6f29c49
Padma Channal <padmahchannal@gmail.com>
2017-11-22 17:27:57 -0600

    Add files via upload

---
+++ Channal_PathOfLight/index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <title>Path of Light</title>
+        <link rel="stylesheet" type="text/css" href="yoga.css">
+    </head>
+  
+  
+    <body>
+      <div class = "wrapper">
+
+        <div class = "header">
+          <h1> Path of Light Yoga Studio </h1>
+        </div>
+
+         <div class = "menu col-2">
+          <ul>
+            <li>
+              <a href="http://localhost:1234/Channal_PathOfLight/Index.php">Home</a>  
+            </li>
+            <li>
+              <a href="http://localhost:1234/Channal_PathOfLight/Classes.php"> Classes</a>
+            </li> 
+            <li>
+              <a href="http://localhost:1234/Channal_PathOfLight/Schedule.php">Schedule</a> 
+            </li>
+            <li>
+              <a href="http://localhost:1234/Channal_PathOfLight/Register.php">Register</a>  
+            </li>
+            <li>
+              <a href="http://localhost:1234/Channal_PathOfLight/Contact.php" >Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class = "col-3">
+          <div class = "row-1">
+            <img class = indeximage src="yogadoor2.jpg" alt="yogadoor2" >
+          </div>
+          <div class = "row-3">
+          	<p>
+            <i>Path of Light Yoga studio </i> <br>
+           
+              612 Serinity way <br>
+              ElDorado, CA 96162 <br><br>
+
+              888-555-5555
+            </p>
+          </div>
+        </div>
+
+        <div class = "col-7">
+          <div class = "row-2">
+            <h2> Find Your Inner Light</h2>
+            <p title="About Yoga Studio"> 
+              <i>Path of Light Yoga Studio </i> provides all levels of yoga practice in tranquil, peaceful environment.Whether you are new to Yoga or experienced practitioner, our dedicated instructors can develop a practice to meet your needs. Let your inner light shine at Path of Light Yoga studio.
+            </p>
+            <ul class = "amenities">
+              <li> Hatha, Vinyasa and Restorative Yoga.</li>
+              <li>Drop-ins welcome.</li>
+              <li>Mats, blocks, and blankets provided.</li>
+              <li> Relax in our Serinity lounge before or after your class.</li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- Constant height? : put a row-4 -->
+        <div class = "col-12 footer">
+          
+          <i>Copyright @2017 Path of Light Yoga Studio</i>
+           </br>
+          <a href="mailto:padmavati@channal.com"> padmavati@channal.com </a>
+          
+        </div>
+    </div>
+  </body>
+</html>
